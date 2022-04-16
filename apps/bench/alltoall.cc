e943175a86561c4448d531f35b909ffa7f9e55f9
Midhul <midhul.v@gmail.com>
2022-04-16 19:40:41 -0400

    all to all

---
+++ apps/bench/alltoall.cc
@@ -0,0 +1,212 @@
+extern "C" {
+#include <base/log.h>
+#include <net/ip.h>
+}
+#undef min
+#undef max
+
+#include "runtime.h"
+#include "thread.h"
+#include "sync.h"
+#include "timer.h"
+#include "net.h"
+#include "fake_worker.h"
+#include "proto.h"
+
+#include <iostream>
+#include <iomanip>
+#include <utility>
+#include <memory>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <random>
+
+namespace {
+
+using sec = std::chrono::duration<double, std::micro>;
+
+const int kRxBufSize = 262144;
+const double kBandwidth = 100e9;
+
+// the number of worker threads to spawn.
+int tx_threads;
+// int rx_threads;
+
+netaddr peer_addr[16];
+
+int num_peers;
+
+// duration of experiment
+int duration;
+
+// load
+double load;
+
+int flow_size;
+
+// self index in peer list
+int my_idx;
+
+void ServerWorker(std::unique_ptr<rt::TcpConn> c) {
+  char buf[kRxBufSize];
+
+  while (true) {
+    // Read data
+    ssize_t ret = c->Read(&buf, kRxBufSize);
+    if (ret <= 0 || ret > kRxBufSize) {
+      if (ret == 0 || ret == -ECONNRESET) break;
+      panic("read failed, ret = %ld", ret);
+    }
+  }
+}
+
+void ServerHandler() {
+  std::unique_ptr<rt::TcpQueue> q(rt::TcpQueue::Listen({0, kNetbenchPort},
+				  4096));
+  if (q == nullptr) panic("couldn't listen for connections");
+
+  while (true) {
+    rt::TcpConn *c = q->Accept();
+    if (c == nullptr) panic("couldn't accept a connection");
+    rt::Thread([=]{ServerWorker(std::unique_ptr<rt::TcpConn>(c));}).Detach();
+  }
+}
+
+void PoissonWorker(int my_idx, int num_nodes, int flow_size, int duration, double load)
+{
+
+  char buf[kRxBufSize];
+
+  // Seed the random generator.
+  std::mt19937 g(microtime());
+
+  std::vector<std::unique_ptr<rt::TcpConn>> conns;
+
+  int num_peers = 0;
+  for(int i = 0; i < num_nodes; i++) {
+    if(i == my_idx) {
+      continue;
+    }
+    std::unique_ptr<rt::TcpConn> outc(rt::TcpConn::Dial({0, 0}, peer_addr[i]));
+    if (unlikely(outc == nullptr)) panic("couldn't connect to raddr.");
+    conns.emplace_back(std::move(outc));
+    num_peers += 1;
+  }
+
+  std::cout << "Connected to all peers\n";
+
+  std::uniform_int_distribution<> uniform_distr(0, num_peers-1);
+  
+
+  // Create a packet transmit schedule.
+  std::vector<double> sched;
+  // TODO: Check MSS and MTU size if using Jumbo frames
+  std::exponential_distribution<double> rd(kBandwidth * load / (flow_size * 8.0 / 1460 * 1500));
+  std::vector<double> tmp;
+  double total_time = 0;
+  int bytes_sent = 0;
+
+  while(total_time < duration) {
+    double ia_time = rd(g);
+    tmp.push_back(ia_time);
+    total_time += ia_time;
+  }
+
+  sched.push_back(tmp[0]);
+  for (std::vector<double>::size_type j = 1; j < tmp.size(); ++j) {
+    tmp[j] += tmp[j - 1];
+    sched.push_back(static_cast<uint64_t>(tmp[j]*1e6));
+  }
+
+  int sched_idx = 0;
+
+
+  barrier();
+  uint64_t expstart = microtime();
+  barrier();
+
+  while(sched_idx < sched.size()) {
+    barrier();
+    uint64_t now = microtime();
+    barrier();
+
+    if(now - expstart >= duration*1e6) {
+      break;
+    }
+
+    if(now - expstart >= sched[sched_idx]) {
+      // pick random peer
+      int peer_idx = uniform_distr(g);
+      // send flow
+      ssize_t ret = conns[peer_idx]->WriteFull(buf, flow_size);
+      if (ret != flow_size)
+        panic("write failed, ret = %ld", ret);
+      bytes_sent += flow_size;
+      sched_idx += 1;
+    } else {
+      rt::Sleep(sched[sched_idx] - (microtime() - expstart));
+    }
+    
+  }
+
+  std::cout << "Total bytes sent: " << bytes_sent << "\n";
+
+}
+
+
+// int StringToAddr(const char *str, uint32_t *addr) {
+//   uint8_t a, b, c, d;
+
+//   if(sscanf(str, "%hhu.%hhu.%hhu.%hhu", &a, &b, &c, &d) != 4)
+//     return -EINVAL;
+
+//   *addr = MAKE_IP_ADDR(a, b, c, d);
+//   return 0;
+// }
+
+void MainHandler(void *arg) {
+  
+  // Start server thread
+  rt::Thread([=]{ServerHandler();}).Detach();
+
+  // Start TX threads
+  std::vector<rt::Thread> ths;
+  for(int i = 0; i < tx_threads; i++) {
+    ths.emplace_back(rt::Thread([=]{(PoissonWorker(my_idx, num_peers, flow_size, duration, load/tx_threads));}));
+  }
+
+  for (auto &t : ths) t.Join();
+
+}
+
+} // anonymous namespace
+
+int main(int argc, char *argv[]) {
+  
+  str_to_netaddr("192.168.11.116:8001", &peer_addr[0]);
+  str_to_netaddr("192.168.11.117:8001", &peer_addr[0]);
+
+  int ret;
+
+  if (argc < 7) {
+    std::cerr << "usage: [cfg_file] [num_peers] [flow-size] [tx-threads] [duration] [load]" << std::endl;
+    return -EINVAL;
+  }
+
+  num_peers = std::stoi(argv[2], nullptr, 0);
+  flow_size = std::stoi(argv[3], nullptr, 0);
+  tx_threads = std::stoi(argv[4], nullptr, 0);
+  duration = std::stoi(argv[5], nullptr, 0);
+  load = std::stod(argv[6], nullptr);
+
+  ret = runtime_init(argv[1], MainHandler, NULL);
+  if (ret) {
+    printf("failed to start runtime\n");
+    return ret;
+  }
+
+  return 0;
+
+}
